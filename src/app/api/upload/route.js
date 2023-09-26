b30f7b0b330c367677b1b3f4a19e3c7bd42a00a4
Muhammad Sajeel <115151677+sajeel45@users.noreply.github.com>
2023-09-26 18:37:35 +0500

    commit

---
+++ src/app/api/upload/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import {writeFile} from "fs/promises";
+
+export async function POST(request){
+    const data = await request.formData();
+    const file = data.get('file');
+    if(!file){
+        return NextResponse.json({"message":"no-image found",success:false})
+    }
+    const byteData = await file.arrayBuffer();
+    const buffer = Buffer.from(byteData);
+    const path = `./public/${file.name}`;
+    await writeFile(path,buffer);
+    return NextResponse.json({"message":"file-uploaded ",success:true});
+}
